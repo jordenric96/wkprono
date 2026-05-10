d45d6bc59ef7dde784104146befddfa230cf2546
jordenric96 <jordenricour96@gmail.com>
2026-05-10 11:21:35 +0000

    landen

--- lib/data.ts
+++ lib/data.ts
@@ -1,89 +1,12 @@
 // --- 48 LANDEN (Het nieuwe WK 2026 format) ---
-// --- 48 LANDEN (Met Vlaggen) ---
 export const WK_LANDEN = [
-  '🇩🇿 Algerije', '🇦🇷 Argentinië', '🇦🇺 Australië', '🇧🇪 België', '🇧🇷 Brazilië', '🇨🇦 Canada', '🇨🇱 Chili', 
-  '🇨🇴 Colombia', '🇨🇷 Costa Rica', '🇩🇰 Denemarken', '🇩🇪 Duitsland', '🇪🇨 Ecuador', '🇪🇬 Egypte', '🏴󠁧󠁢󠁥󠁮󠁧󠁿 Engeland', 
-  '🇫🇷 Frankrijk', '🇬🇭 Ghana', '🇭🇺 Hongarije', '🇮🇷 Iran', '🇮🇹 Italië', '🇨🇮 Ivoorkust', '🇯🇵 Japan', 
-  '🇨🇲 Kameroen', '🇭🇷 Kroatië', '🇲🇱 Mali', '🇲🇦 Marokko', '🇲🇽 Mexico', '🇳🇱 Nederland', '🇳🇬 Nigeria', 
-  '🇺🇦 Oekraïne', '🇦🇹 Oostenrijk', '🇵🇾 Paraguay', '🇵🇪 Peru', '🇵🇱 Polen', '🇵🇹 Portugal', '🇶🇦 Qatar', 
-  '🇸🇦 Saudi-Arabië', '🏴󠁧󠁢󠁳󠁣󠁴󠁿 Schotland', '🇸🇳 Senegal', '🇷🇸 Servië', '🇪🇸 Spanje', '🇹🇷 Turkije', '🇺🇾 Uruguay', 
-  '🇻🇪 Venezuela', '🇺🇸 Verenigde Staten', '🏴󠁧󠁢󠁷󠁬󠁳󠁿 Wales', '🇿🇦 Zuid-Afrika', '🇰🇷 Zuid-Korea', '🇸🇪 Zweden', '🇨🇭 Zwitserland'
-].sort();
-
-// --- GIGANTISCHE SPELERSLIJST ---
-export const TOP_SPELERS = [
-  // --- BELGIË (Rode Duivels) ---
-  'Kevin De Bruyne', 'Romelu Lukaku', 'Lois Openda', 'Jeremy Doku', 'Leandro Trossard', 
-  'Charles De Ketelaere', 'Johan Bakayoko', 'Amadou Onana', 'Youri Tielemans', 'Orel Mangala', 
-  'Arthur Vermeeren', 'Aster Vranckx', 'Dodi Lukebakio', 'Timothy Castagne', 'Wout Faes', 
-  'Zeno Debast', 'Arthur Theate', 'Maxim De Cuyper', 'Thomas Meunier', 'Julien Duranville', 'Romeo Lavia',
-
-  // --- NEDERLAND (Oranje) ---
-  'Virgil van Dijk', 'Matthijs de Ligt', 'Nathan Aké', 'Denzel Dumfries', 'Jeremie Frimpong', 
-  'Ian Maatsen', 'Stefan de Vrij', 'Micky van de Ven', 'Lutsharel Geertruida', 'Daley Blind',
-  'Frenkie de Jong', 'Teun Koopmeiners', 'Tijjani Reijnders', 'Jerdy Schouten', 'Xavi Simons', 
-  'Joey Veerman', 'Ryan Gravenberch', 'Georginio Wijnaldum', 'Marten de Roon', 'Quinten Timber',
-  'Cody Gakpo', 'Memphis Depay', 'Donyell Malen', 'Wout Weghorst', 'Brian Brobbey', 
-  'Joshua Zirkzee', 'Steven Bergwijn', 'Noa Lang',
-
-  // --- FRANKRIJK ---
-  'Kylian Mbappé', 'Antoine Griezmann', 'Ousmane Dembélé', 'Olivier Giroud', 'Marcus Thuram', 
-  'Randal Kolo Muani', 'Kingsley Coman', 'Bradley Barcola', 'Aurelien Tchouameni', 
-  'Eduardo Camavinga', 'Adrien Rabiot', 'N\'Golo Kanté', 'William Saliba', 'Dayot Upamecano', 
-  'Theo Hernandez', 'Jules Koundé',
-
-  // --- ENGELAND ---
-  'Harry Kane', 'Jude Bellingham', 'Bukayo Saka', 'Phil Foden', 'Ollie Watkins', 
-  'Cole Palmer', 'Marcus Rashford', 'Jack Grealish', 'Declan Rice', 'Trent Alexander-Arnold', 
-  'Kyle Walker', 'John Stones', 'Kieran Trippier', 'Ivan Toney', 'Anthony Gordon', 'Kobbie Mainoo',
-
-  // --- SPANJE ---
-  'Rodri', 'Pedri', 'Gavi', 'Lamine Yamal', 'Alvaro Morata', 'Nico Williams', 
-  'Dani Olmo', 'Mikel Oyarzabal', 'Ferran Torres', 'Dani Carvajal', 'Aymeric Laporte', 
-  'Fabian Ruiz', 'Martin Zubimendi',
-
-  // --- DUITSLAND ---
-  'Jamal Musiala', 'Florian Wirtz', 'Leroy Sané', 'Kai Havertz', 'Joshua Kimmich', 
-  'Ilkay Gündogan', 'Antonio Rüdiger', 'Serge Gnabry', 'Niclas Füllkrug', 'Jonathan Tah', 
-  'Toni Kroos', 'Nico Schlotterbeck',
-
-  // --- PORTUGAL ---
-  'Cristiano Ronaldo', 'Bruno Fernandes', 'Bernardo Silva', 'Rafael Leão', 'João Félix', 
-  'Diogo Jota', 'Rúben Dias', 'João Cancelo', 'Vitinha', 'João Palhinha', 'Gonçalo Ramos', 
-  'Pedro Neto',
-
-  // --- ITALIË ---
-  'Federico Chiesa', 'Nicolò Barella', 'Sandro Tonali', 'Alessandro Bastoni', 
-  'Lorenzo Pellegrini', 'Gianluca Scamacca', 'Davide Frattesi', 'Federico Dimarco',
-
-  // --- ARGENTINIË ---
-  'Lionel Messi', 'Lautaro Martínez', 'Julián Álvarez', 'Enzo Fernández', 'Alexis Mac Allister', 
-  'Alejandro Garnacho', 'Paulo Dybala', 'Rodrigo De Paul', 'Cristian Romero', 'Lisandro Martínez',
-
-  // --- BRAZILIË ---
-  'Vinícius Júnior', 'Rodrygo', 'Raphinha', 'Endrick', 'Gabriel Martinelli', 'Neymar', 
-  'Lucas Paquetá', 'Bruno Guimarães', 'Casemiro', 'Marquinhos', 'Eder Militão', 'Gabriel Jesus',
-
-  // --- REST VAN DE WERELD (Sterspelers) ---
-  'Erling Haaland', 'Robert Lewandowski', 'Dusan Vlahovic', 'Heung-min Son', 'Mohamed Salah', 
-  'Luis Díaz', 'Darwin Núñez', 'Federico Valverde', 'Victor Osimhen', 'Achraf Hakimi', 
-  'Khvicha Kvaratskhelia', 'Alphonso Davies', 'Christian Pulisic', 'Kaoru Mitoma', 
-  'Serhou Guirassy', 'Brahim Díaz', 'Alexander Isak'
-].sort();
-
-// --- UITGEBREIDE KEEPERSLIJST ---
-export const TOP_KEEPERS = [
-  'Thibaut Courtois', 'Koen Casteels', 'Matz Sels', 'Thomas Kaminski',
-  'Bart Verbruggen', 'Justin Bijlow', 'Mark Flekken', 'Nick Olij',
-  'Alisson Becker', 'Ederson', 'Bento',
-  'Mike Maignan', 'Alphonse Areola', 'Brice Samba',
-  'Emiliano Martínez', 'Gerónimo Rulli',
-  'Gianluigi Donnarumma', 'Guglielmo Vicario', 'Alex Meret',
-  'Jordan Pickford', 'Aaron Ramsdale', 'Dean Henderson',
-  'Manuel Neuer', 'Marc-André ter Stegen', 'Alexander Nübel',
-  'Unai Simón', 'David Raya', 'Álex Remiro',
-  'Diogo Costa', 'Rui Patrício',
-  'Jan Oblak', 'Yann Sommer', 'Gregor Kobel', 
-  'Wojciech Szczęsny', 'Dominik Livaković', 'Yassine Bounou', 
-  'Giorgi Mamardashvili', 'Andriy Lunin', 'Keylor Navas'
-].sort();
+  '🇩🇿 Algerije', '🇦🇷 Argentinië', '🇦🇺 Australië', '🇧🇪 België', '🇧🇦 Bosnië', '🇧🇷 Brazilië', 
+  '🇨🇦 Canada', '🇨🇴 Colombia', '🇨🇩 Congo', '🇨🇼 Curaçao', '🇩🇪 Duitsland', '🇪🇨 Ecuador', 
+  '🇪🇬 Egypte', '🏴󠁧󠁢󠁥󠁮󠁧󠁿 Engeland', '🇫🇷 Frankrijk', '🇬🇭 Ghana', '🇭🇹 Haïti', '🇮🇶 Irak', '🇮🇷 Iran', 
+  '🇨🇮 Ivoorkust', '🇯🇵 Japan', '🇯🇴 Jordanië', '🇨🇻 Kaapverdië', '🇭🇷 Kroatië', '🇲🇦 Marokko', 
+  '🇲🇽 Mexico', '🇳🇱 Nederland', '🇳🇿 Nieuw-Zeeland', '🇳🇴 Noorwegen', '🇺🇿 Oezbekistan', 
+  '🇦🇹 Oostenrijk', '🇵🇦 Panama', '🇵🇾 Paraguay', '🇵🇹 Portugal', '🇶🇦 Qatar', '🇸🇦 Saudi-Arabië', 
+  '🏴󠁧󠁢󠁳󠁣󠁴󠁿 Schotland', '🇸🇳 Senegal', '🇪🇸 Spanje', '🇨🇿 Tsjechië', '🇹🇳 Tunesië', '🇹🇷 Turkije', 
+  '🇺🇾 Uruguay', '🇺🇸 Verenigde Staten', '🇿🇦 Zuid-Afrika', '🇰🇷 Zuid-Korea', '🇸🇪 Zweden', 
+  '🇨🇭 Zwitserland'
+];
